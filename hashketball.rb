a670b76f6c3b4da47ea4db836e202272290e9f67
Aian Soktoev <soktoevayan@gmail.com>
2020-06-05 17:27:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,44 @@
-def game_hash()
-  
-  newHash = {}
-  newHash[:home] = {}
-  newHash[:away] = {}
-  newHash[:home][:team_name] = "String"
-  newHash[:home][:colors] = []
-  newHash[:home][:players] = [] #AofH
-  newHash[:home][:players][i][key] = "Symbol"
-  newHash[:away][:team_name] = "String"
-  newHash[:away][:colors] = []
-  newHash[:away][:players] = []
-  newHash[:away][:players][j][key] = "Symbol"
-end   
+def game_hash
+  my_data = {
+    :home => {
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black", "White"]
+      :players => [{
+        :player_name => "Alan Anderson"
+        :number => "0"
+        :shoe => "16"
+        :points => "22"
+        :rebounds => "12"
+        :assists => "12"
+        :steals => "3"
+        :blocks => "1"
+        :slam_dunks => "1"
+      }, {
+        :player_name => "Reggie Evans"
+        :number => "30"
+        :shoe => "14"
+        :points => "12"
+        :rebounds => "12"
+        :assists => "12"
+        :steals => "12"
+        :blocks => "12"
+        :slam_dunks => "7"
+      }, ]
+    }, 
+    :away => {
+      :team_name => ""
+      :colors => []
+      :players => [{
+        :player_name =>
+        :number =>
+        :shoe => 
+        :points =>
+        :rebounds =>
+        :assists => 
+        :steals =>
+        :blocks =>
+        :slam_dunks => 
+      }]
+    }
+  }
+end  
