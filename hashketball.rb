4a1a12927a5e8b05efdadd0612f2a22521f0c430
Aian Soktoev <soktoevayan@gmail.com>
2020-06-05 17:10:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,13 @@
-# Write your code here!
+def game_hash()
+  newHash = {}
+  newHash[:home] = {}
+  newHash[:away] = {}
+  newHash[:home][:team_name] = "String"
+  newHash[:home][:colors] = []
+  newHash[:home][:players] = [] #AofH
+  newHash[:home][:players][i][key] = "Symbol"
+  newHash[:away][:team_name] = "String"
+  newHash[:away][:colors] = []
+  newHash[:away][:players] = []
+  newHash[:away][:players][j][key] = "Symbol"
+end   
